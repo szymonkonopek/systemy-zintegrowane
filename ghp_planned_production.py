d2149bc11783dcdf0e71032139be063dfdc4e6ce
AmeliaLis <lis.amelia.maria@gmail.com>
2024-04-04 22:36:29 +0200

    Update to use json files, not python's input

--- ghp_planned_production.py
+++ ghp_planned_production.py
@@ -15,10 +15,6 @@ def read_json_file(file_path):
         print(f"Error: '{file_path}' does not contain valid JSON.")
         return None
 
-# Input parameters responsible for GHP - Główny Harmonogram Produkcji (eng. MPS - master production schedule)
-current_on_hand = int(input("Enter current number of products on hand: "))
-lead_assemble_time = int(input("Enter lead assemble time for chair: "))
-
 # Read json data from planned_orders.json
 json_data = read_json_file('planned_orders.json')
 if json_data:
@@ -27,16 +23,19 @@ if json_data:
     transposed_df = data_pd.transpose()
     print(transposed_df.to_string(header=False))
 
-planned_production = []
+# Input storage data
+storage_data = read_json_file('storage.json')
+if storage_data:
+    print("Storage Data:")
+    data_pd = pd.read_json("storage.json")
+    transposed_df = data_pd.transpose()
+    print(transposed_df.to_string(header=False))
 
-for week in range(0,len(json_data['week'])):
-    planned_production.append(int(input(f"Enter YOUR planned production for week nr {week+1}: ")))
 
-# Sample data before calculation
-json_data['planned_production'] = planned_production
-json_data['on_hand'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# Input parameters responsible for GHP - Główny Harmonogram Produkcji (eng. MPS - master production schedule)
+current_on_hand = storage_data["chairs"]["initial_quantity"]
+lead_assemble_time = storage_data["chairs"]["waiting_time_in_weeks"]
 
-data_pd['planned production'] = planned_production
 transposed_df = data_pd.transpose()
 print("Planned orders and planned production:")
 print(transposed_df.to_string(header=False))
@@ -48,6 +47,7 @@ for week in range(0, len(json_data['week'])):
 
     # Check if there is no orders planned
     if json_data['orders'][week] == 0:
+        # Planned_production field can be entered by user, but if the planned_production will not be enough for orders, the rest production will be calculated automatically
         if json_data['planned_production'][week] == 0:
             # If there is any production planned by user, then calculate the rest
             if json_data['week'][week] == 1:
@@ -71,6 +71,7 @@ for week in range(0, len(json_data['week'])):
                 # as on first week, we can't go back and produce any more product on week 0
                 # so we need to use the current_on_hand value, entered by user
                 if (current_on_hand < json_data['orders'][week]) and json_data['planned_production'][week] == 0:
+                    # the planned production condition is checked if it was setup by user, so the user's input won't be omitted
                     json_data['planned_production'][week] = json_data['orders'][week] - current_on_hand
                 on_hand.append(current_on_hand - json_data['orders'][week] + json_data['planned_production'][week])
             else:
@@ -87,10 +88,7 @@ for week in range(0, len(json_data['week'])):
 with open('planned_orders_ghp_test.json', 'w') as f:
     json.dump(json_data, f, indent=2)
 
-
 data_pd = pd.read_json("planned_orders_ghp_test.json")
 transposed_df = data_pd.transpose()
 print("Final MPS structure:")
 print(transposed_df.to_string(header=False))
-
-# fix when there is no on_hand and planned equals 0
