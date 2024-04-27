4db7f3d5aa55b68f5bca770f34b6777429da0df2
malwinakochman <malwinakochman@gmail.com>
2024-04-27 16:15:17 +0200

    addictional info done

--- dictionary.py
+++ dictionary.py
@@ -2,8 +2,8 @@ from collections import OrderedDict
 
 production_data_ghp_map = OrderedDict({
     'demand': 'Demand',
-    'on_hand': 'On Hand',
-    'planned_production': 'Planned Production'
+    'planned_production': 'Planned Production',
+    'on_hand': 'On Hand'
 })
 
 production_data_map = OrderedDict({
@@ -13,6 +13,18 @@ production_data_map = OrderedDict({
     'net_requirements': 'Net Requirements',
     'planned_order_releases': 'Planned Order Releases',
     'planned_order_receipts': 'Planned Order Receipts',
+})
+
+storage_ghp_data_map = OrderedDict({
+    'waiting_time_in_weeks': 'Waiting Time in Weeks',
+    'initial_quantity': 'Initial Quantity'    
+})
+
+storage_data_map = OrderedDict({
+    'waiting_time_in_weeks': 'Waiting Time in Weeks',
+    'units_per_batch': 'Units per Batch',
+    'level': 'Level',
+    'initial_quantity': 'Initial Quantity',
 })
 
 table_file_name_map = OrderedDict({
--- my_dash_app.py
+++ my_dash_app.py
@@ -35,6 +35,20 @@ def prepare_table_data(json_data, production_data_map):
             })
         return [{'Production Data': attr, **values} for attr, values in data_by_attribute.items()]
     return []
+1
+def prepare_storage_table_data(id, data_map):
+    json_data = read_json_file('storage.json')
+    
+    if json_data and id in json_data:
+        storage_data = json_data[id]
+        data = []
+        for attr in data_map:
+            if attr in storage_data:
+                data.append({'Attribute': data_map[attr], 'Value': storage_data[attr]})
+        return data
+    else:
+        print(f"Error: Data for ID '{id}' not found in storage.")
+        return []
 
 def create_table(file_name, map_name, table_name, editable):
     json_data = read_json_file(file_name)
@@ -47,7 +61,6 @@ def create_table(file_name, map_name, table_name, editable):
     table_id = file_name.replace('.json', '') + '_table'
     div_id = file_name.replace('.json', '') + '_div'
 
-    # Stwórz oddzielny kontener dla tabeli i wyników
     table_container = html.Div([
         html.H1(table_name),
         dash_table.DataTable(
@@ -56,13 +69,15 @@ def create_table(file_name, map_name, table_name, editable):
             data=data_rows,
             editable=editable
         ),
-        html.Div(id='output_' + div_id)
+        html.Div(id='output_' + div_id),
     ])
     
-    @app.callback(
-        Output('output_' + div_id, 'children'),
-        [Input(table_id, 'data')]
-    )
+    # Tutaj to jest tylko po to żeby wyświetlić jsona jak on się zmienia, ale jest to na razie nie potrzebne
+    
+    # @app.callback(
+    #     Output('output_' + div_id, 'children'),
+    #     [Input(table_id, 'data')]
+    # )
     def update_output(rows):
         df1 = pd.DataFrame(rows)
         return html.Div([
@@ -72,12 +87,45 @@ def create_table(file_name, map_name, table_name, editable):
     
     return table_container
 
+def create_storage_table(id, map_name, editable):
+    table_data = prepare_storage_table_data(id, map_name)
+
+    columns = [{'name': '', 'id': 'Attribute'},
+               {'name': 'Value', 'id': 'Value'}]
+
+    table_id = f"{id}_storage_table"
+
+    table_container = html.Div([
+        dash_table.DataTable(
+            id=table_id,
+            columns=columns,
+            data=table_data,
+            style_header={'display': 'none'},
+            style_data={
+            'width': 135,
+            },
+            fill_width=False,
+            css=[ { 'selector': 'tr:first-child', 'rule': 'display: none'},],
+            
+        ),
+    ])
+    
+    return table_container
     
 def runDashApp():
     app.layout = html.Div([
     create_table("planned_order.json", production_data_ghp_map, "Initial GHP Data:", True),
+    create_storage_table("chairs", storage_ghp_data_map, True),
     create_table("planned_orders_ghp_summary.json", production_data_ghp_map, "Final GHP structure:", True),
-    create_table("padding.json", production_data_map, "MRP Data: padding", True)
+    create_storage_table("chairs", storage_ghp_data_map, True),
+    create_table("padding.json", production_data_map, "MRP Data: padding", True),
+    create_storage_table("padding", storage_data_map, True),
+    create_table("frame.json", production_data_map, "MRP Data: frame", True),
+    create_storage_table("frame", storage_data_map, True),
+    create_table("nails.json", production_data_map, "MRP Data: nails", True),
+    create_storage_table("frame", storage_data_map, True),
+    create_table("wooden_construction_elements.json", production_data_map, "MRP Data: wooden construction elements", True),
+    create_storage_table("wooden_construction_elements", storage_data_map, True),
     ])
     
     app.run_server(debug=True)    
